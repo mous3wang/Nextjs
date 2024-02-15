61afd3e41c59bd6bfa2a2e9475f970d053046db4
mous3 <mous3.wang@gmail.com>
2024-02-15 17:51:16 +0900

    customers

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,22 @@
-export default function Page() {
-    return <p>Customers Page</p>;
+import { Suspense } from "react";
+import { fetchFilteredCustomers } from "../../lib/data";
+import CustomersTable from "../../ui/customers/table";
+import { lusitana } from "../../ui/fonts";
+import Search from "../../ui/search";
+
+
+export default async function Page({
+    searchParams,
+}:{
+    searchParams?:{
+        query?:string
+    }
+}) {
+    const query = searchParams?.query || '';
+    const customers = await fetchFilteredCustomers(query);
+    return (
+        <div className="w-full">        
+            <CustomersTable customers={customers} />            
+        </div>
+    )
 }
